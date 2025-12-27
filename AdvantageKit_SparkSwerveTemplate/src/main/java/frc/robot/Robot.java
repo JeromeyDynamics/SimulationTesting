974230c2c5ca3ca0e536f6fc8e808400b0310f29
Jerome Hillock <jeromeydawinner@gmail.com>
2025-12-27 16:33:50 -0500

    moved the spark template into the folder so that the simulation would open

--- AdvantageKit_SparkSwerveTemplate/src/main/java/frc/robot/Robot.java
+++ AdvantageKit_SparkSwerveTemplate/src/main/java/frc/robot/Robot.java
@@ -13,6 +13,7 @@
 
 package frc.robot;
 
+import edu.wpi.first.wpilibj.Threads;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 import org.littletonrobotics.junction.LogFileUtil;
